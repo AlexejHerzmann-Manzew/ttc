3671350ac1a0dc6ef2d2e5a3b8fa8766edff2084
Юрий <@Whizzpered.Dlink>
2015-03-19 19:54:00 +0400

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/org/ttc/core/game/Player.java
@@ -0,0 +1,34 @@
+package org.ttc.core.game;
+
+import java.io.Serializable;
+import java.util.Random;
+import org.newdawn.slick.Color;
+
+/**
+ *
+ * @author yew_mentzaki
+ */
+public class Player implements Serializable {
+
+    public boolean ai;
+
+    public Player(String name, int color) {
+        this.name = name;
+        this.color = new Color(color);
+        this.camerax = 0;
+        this.cameray = 0;
+    }
+    public String name;
+    public Color color = Color.decode("#ffffff");
+    public int colorn = 0;
+    public int mode = 0;
+
+    public int type() {
+        if (mode == 0) {
+            return Unit.r.nextInt(4);
+        }else{
+            return mode - 1;
+        }
+    }
+    int camerax, cameray;
+}
